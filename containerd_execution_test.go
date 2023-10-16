25d7fc41db420252e11587d93cbc8c76ad1438a3
austinarbor-wk <austin.arbor@workiva.com>
2023-10-16 15:56:23 -0400

    add test for building the nerdctl args

--- containerd_execution_test.go
+++ containerd_execution_test.go
@@ -119,3 +119,87 @@ func Test_createTask_cleanup_ErrNotIgnored(t *testing.T) {
 	mockTask.AssertExpectations(t)
 	mockContainer.AssertNotCalled(t, "Delete", mock.Anything, mock.Anything)
 }
+
+func Test_createTask_buildCreateContainerArgs(t *testing.T) {
+	client := new(client)
+	cd := Containerd{
+		ContainerdClient: client,
+		Namespace:        "k8s.io",
+	}
+
+	task := &createTask{
+		opts: CreateTaskOptions{
+			Mounts: []specs.Mount{
+				{
+					Destination: "/go/src",
+					Source:      "/command/1",
+					Type:        "bind",
+				},
+				{
+					Destination: "/opt/libs/1",
+					Source:      "/plugin/1/2",
+					Type:        "bind",
+					Options:     []string{"ro", "ab"},
+				},
+			},
+			Env: []string{
+				"A=B",
+				"C=D",
+			},
+			User: "61000",
+			CommandDetails: CommandDetails{
+				ChainExecutorId: 1,
+				ExecutorId:      2,
+				ResultId:        3,
+			},
+			Image: "docker-agent:latest",
+		},
+		labels: map[string]string{
+			"unit-test": "true",
+		},
+	}
+
+	args := task.buildCreateContainerArgs(cd)
+	for i := 0; i < len(args); i++ {
+		var assertion func(t assert.TestingT, a any, b any, msgAndArgs ...any) bool = assert.Equal
+		var firstArg interface{}
+		var secondArg interface{} = args[i]
+		switch i {
+		case 0:
+			firstArg = "--namespace"
+		case 1:
+			firstArg = cd.Namespace
+		case 2:
+			firstArg = "create"
+		case 3:
+			firstArg = "--name"
+		case 4:
+			firstArg = args[i]
+			secondArg = "chains-1-2-3-"
+			assertion = assert.Contains
+		case 5:
+			firstArg = "--user"
+		case 6:
+			firstArg = "61000"
+		case 7, 9:
+			firstArg = "-v"
+		case 8:
+			firstArg = "/command/1:/go/src"
+		case 10:
+			firstArg = "/plugin/1/2:/opt/libs/1:ro,ab"
+		case 11, 13:
+			firstArg = "-e"
+		case 12:
+			firstArg = "A=B"
+		case 14:
+			firstArg = "C=D"
+		case 15:
+			firstArg = "--label"
+		case 16:
+			firstArg = "unit-test=true"
+		case 17:
+			firstArg = task.opts.Image
+		}
+		assertion(t, firstArg, secondArg)
+	}
+}
